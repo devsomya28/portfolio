f0856cc6e94d65464ad78131683722f1e094dfed
Somya Devda <devsomya@gmail.com>
2026-03-06 21:08:44 +0530

    message

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return <h1>My Portfolio</h1>;
-}
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
